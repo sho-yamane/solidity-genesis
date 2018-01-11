34f9b250ea49f89544a1e465ef3269d7a3db2f65
Sho Yamane <sns.yamane@gmail.com>
2018-01-11 17:40:52 +0900

    save

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -14,6 +14,7 @@ contract CrowdFunding {
     bool public ended;          // キャンペーンが終了しているかどうか
     uint public goalAmount;     // 目標額
     uint public totalAmount;    // 投資の総額
+    bool public to;             // 送金できたかどうか
     mapping (uint => Investor) public investors; // 投資家管理用のマップ
 
     modifier onlyOwner () {
@@ -22,7 +23,7 @@ contract CrowdFunding {
     }
 
     /// コンストラクタ（durationは期間）
-    function CrowdFunding(uint _duration, uint _goalAmount) {
+    function CrowdFunding(uint _duration, uint _goalAmount) public {
         owner = msg.sender;
 
         // 締め切りをUnixtimeで設定
@@ -37,11 +38,11 @@ contract CrowdFunding {
     }
 
     /// 投資する際に呼出される関数
-    function fund() payable {
+    function fund() public payable {
         // キャンペーンが終わってたら処理を中断
         require(!ended); // Falseの場合終了
 
-        Investor inv = investors[numInvestors++]
+        Investor storage inv = investors[numInvestors++];
         inv.addr = msg.sender;
         inv.amount = msg.value;
         totalAmount += inv.amount;
@@ -55,11 +56,27 @@ contract CrowdFunding {
 
         // 締切の前の場合は処理を中断する
         require(now >= deadline); // Falseの場合終了
-        if(totalAmount >= goalAmount) {
+        if (totalAmount >= goalAmount) {
             status = "Campaign Succeed";
             ended = true;
             // オーナーにコントラクト内のすべてのetherを送金する
+            to = owner.send(this.balance);
+            require(to);
+        } else {
+            uint i = 0;
+            status = "Campaign Failed";
+            ended = true;
+
+            // 投資家毎にetherを返金する
+            while(i <= numInvestors) {
+                to = investors[i].addr.send(investors[i].amount);
+                require(to);
+                i++;
+            }
         }
     }
 
+    function kill() public onlyOwner {
+        selfdestruct(owner);
+    }
 }
